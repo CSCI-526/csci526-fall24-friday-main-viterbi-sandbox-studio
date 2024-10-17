975b6cd7429752d3732116e72550cc973993cba3
Komorebi452 <qsy0710@gmail.com>
2024-10-17 00:52:12 -0700

    Updated Script

--- Assets/Script/GameComplete.cs
+++ Assets/Script/GameComplete.cs
@@ -5,22 +5,23 @@ using TMPro;
 
 public class GameComplete : MonoBehaviour
 {
-    public TextMeshProUGUI winText; 
-    private int reached = 0;    
-    
+    public TextMeshProUGUI winText;
+    private int reached = 0;
+    private HashSet<GameObject> reachedPlayers = new HashSet<GameObject>();
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && !reachedPlayers.Contains(other.gameObject))
         {
             reached++;
-            
+            reachedPlayers.Add(other.gameObject);
+
             if (reached == 2)
             {
                 winText.enabled = true;
             }
         }
-        
+
     }
-    
-    
+
+
 }
