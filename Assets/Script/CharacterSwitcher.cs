283c1330cc6052faf3e65f7b0fc676480b66cc90
zye298 <zye298@gmail.com>
2024-11-05 18:48:19 -0800

    tutoriallevel design
    and character switcher script change

--- Assets/Script/CharacterSwitcher.cs
+++ Assets/Script/CharacterSwitcher.cs
@@ -5,25 +5,31 @@ using UnityEngine;
 public class CharacterSwitcher : MonoBehaviour
 {
     public GameObject player1;    // the first character
-    public GameObject player2;    // the second character
+    public GameObject player2;    // the second character (optional)
     public Camera mainCamera;     // main camera
 
     private GameObject activePlayer;
-
     private bool isControllingBoth = false;
 
     private CharacterSwitchTracker characterSwitchTracker;
 
     void Start()
     {
-        ActivatePlayer(player2, player1);
+        if (player2 != null)
+        {
+            ActivatePlayer(player2, player1);  // Start with player2 active by default
+        }
+        else
+        {
+            ActivatePlayer(player1, null);  // Only player1 exists
+        }
+
         characterSwitchTracker = FindObjectOfType<CharacterSwitchTracker>();
     }
 
     void Update()
     {
-        
-        if (Input.GetKeyDown(KeyCode.R))
+        if (player2 != null && Input.GetKeyDown(KeyCode.R))
         {
             if (activePlayer == player1)
             {
@@ -33,14 +39,14 @@ public class CharacterSwitcher : MonoBehaviour
             {
                 ActivatePlayer(player1, player2);
             }
+
             if (characterSwitchTracker != null)
             {
                 characterSwitchTracker.RecordPlayerSwitch();
             }
-
         }
-        
-        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+
+        if (player2 != null && (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)))
         {
             isControllingBoth = true;
             ControlBothPlayers(true);
@@ -51,40 +57,32 @@ public class CharacterSwitcher : MonoBehaviour
             ControlBothPlayers(false);
         }
 
-        
         UpdateArrowVisibility();
     }
 
-    
     void ActivatePlayer(GameObject newActivePlayer, GameObject oldActivePlayer)
     {
-        
-        if (isControllingBoth) return;
+        if (isControllingBoth || newActivePlayer == null) return;
 
-        
-        oldActivePlayer.GetComponent<PlayerController>().enabled = false;
-        
+        if (oldActivePlayer != null)
+        {
+            oldActivePlayer.GetComponent<PlayerController>().enabled = false;
+        }
+
         newActivePlayer.GetComponent<PlayerController>().enabled = true;
-
-        
         activePlayer = newActivePlayer;
     }
 
-    
     void ControlBothPlayers(bool controlBoth)
     {
-        if (controlBoth)
+        if (player2 == null)
         {
-            
             player1.GetComponent<PlayerController>().enabled = true;
-            player2.GetComponent<PlayerController>().enabled = true;
+            return;
         }
-        else
-        {
-            
-            player1.GetComponent<PlayerController>().enabled = (activePlayer == player1);
-            player2.GetComponent<PlayerController>().enabled = (activePlayer == player2);
-        }
+
+        player1.GetComponent<PlayerController>().enabled = controlBoth || (activePlayer == player1);
+        player2.GetComponent<PlayerController>().enabled = controlBoth || (activePlayer == player2);
     }
 
     public GameObject getActivePlayer()
@@ -92,18 +90,22 @@ public class CharacterSwitcher : MonoBehaviour
         return activePlayer;
     }
 
-    
     void UpdateArrowVisibility()
     {
+        if (player2 == null)
+        {
+            // Only player1 exists, so show its arrow only
+            player1.GetComponent<PlayerController>().ToggleArrow(true);
+            return;
+        }
+
         if (isControllingBoth)
         {
-            
             player1.GetComponent<PlayerController>().ToggleArrow(true);
             player2.GetComponent<PlayerController>().ToggleArrow(true);
         }
         else
         {
-            
             player1.GetComponent<PlayerController>().ToggleArrow(activePlayer == player1);
             player2.GetComponent<PlayerController>().ToggleArrow(activePlayer == player2);
         }
