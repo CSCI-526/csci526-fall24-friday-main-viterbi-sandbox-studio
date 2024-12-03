7180f4ca83ab075b75b76eea1900cd477a45e091
yingtu35 <daniel860305@gmail.com>
2024-12-03 15:38:14 -0800

    add falling reset transition

--- Assets/Script/ResetOutOfBounds.cs
+++ Assets/Script/ResetOutOfBounds.cs
@@ -4,32 +4,36 @@ using UnityEngine;
 
 public class ResetOutOfBounds : MonoBehaviour
 {
-    private float yBoundary = -20f;
-    public GameObject player1;
-    public GameObject player2; // optional
-
     private LevelResetManager levelResetManager;
+    private SceneTransitionManager sceneTransitionManager;
     // Start is called before the first frame update
     void Start()
     {
         levelResetManager = FindObjectOfType<LevelResetManager>();
+        sceneTransitionManager = FindObjectOfType<SceneTransitionManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        if (player1 != null && IsPlayerOutOfBound(player1))
+        if (ShouldTriggerReset(other))
         {
-            levelResetManager.ResetCurrentLevelObjects();
-        }
-        else if (player2 != null && IsPlayerOutOfBound(player2))
-        {
-            levelResetManager.ResetCurrentLevelObjects();
+            StartCoroutine(ResetGame());
         }
     }
 
-    bool IsPlayerOutOfBound(GameObject player)
+    private bool ShouldTriggerReset(Collider other)
     {
-        return player.transform.position.y < yBoundary;
+        return other.CompareTag("Player") ||
+            other.CompareTag("BoxLevelF");
+    }
+
+    private IEnumerator ResetGame()
+    {
+        PersistentMenu.instance.inTransit = true;
+        yield return StartCoroutine(sceneTransitionManager.FadeOut());
+        levelResetManager.ResetCurrentLevelObjects();
+        yield return new WaitForSeconds(1f);
+        PersistentMenu.instance.inTransit = false;
+        yield return StartCoroutine(sceneTransitionManager.FadeIn());
     }
 }
